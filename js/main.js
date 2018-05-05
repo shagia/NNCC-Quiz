6b12e39bf4895c19f6bd1c850f197b5fdbd3ab91
shagia <mrkraiola@gmail.com>
2018-05-04 17:27:08 -0700

    inital-commit

---
+++ js/main.js
@@ -0,0 +1,621 @@
+//Structure initializing
+$(document).ready(function() {
+    $("#Q2").hide();
+    $("#Q3").hide();
+    $("#Q4").hide();
+});
+
+//Q1
+var q1b1State = false;
+var q1b2State = false;
+var q1b3State = false;
+var q1b4State = false;
+var q1b5State = false;
+var q1b6State = false;
+var q1Status = 0;
+
+//Q2
+var q2b1State = false;
+var q2b2State = false;
+var q2b3State = false;
+var q2b4State = false;
+var q2b5State = false;
+var q2b6State = false;
+var q2Status = 0;
+
+//Q3
+var q3b1State = false;
+var q3b2State = false;
+var q3b3State = false;
+var q3b4State = false;
+var q3b5State = false;
+var q3b6State = false;
+var q3b7State = false;
+var q3b8State = false;
+var q3Status = 0;
+
+//Final solutions
+var q1Solution = [];
+var q2Solution = [];
+var q3Solution = [];
+
+
+
+//Q1 statements
+$("#q1b1").click(function() {
+
+    if (q1b1State == false && q1Status < 1) {
+        q1b1State = true;
+        q1Status = q1Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q1Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q1b1State + ', ' + q1Status);
+    } else if (q1b1State == true) {}
+});
+
+
+
+$("#q1b2").click(function() {
+    if (q1b2State == false && q1Status < 1) {
+        q1b2State = true;
+        q1Status = q1Status + 1;
+
+        var buttonContent = $(event.target).text();
+        q1Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        $(this).addClass('active');
+        console.log(this + ' ' + q1b2State + ', ' + q1Status);
+    }
+});
+$("#q1b3").click(function() {
+    if (q1b3State == false && q1Status < 1) {
+        q1b3State = true;
+        q1Status = q1Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q1Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        console.log(this + ' ' + q1b3State + ', ' + q1Status);
+    }
+});
+$("#q1b4").click(function() {
+    if (q1b4State == false && q1Status < 1) {
+        q1b4State = true;
+        q1Status = q1Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q1Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        console.log(this + ' ' + q1b4State + ', ' + q1Status);
+    }
+});
+$("#q1b5").click(function() {
+    if (q1b5State == false && q1Status < 1) {
+        q1b5State = true;
+        q1Status = q1Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q1Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        console.log(this + ' ' + q1b5State + ', ' + q1Status);
+    }
+});
+$("#q1b6").click(function() {
+    if (q1b6State == false && q1Status < 1) {
+        q1b6State = true;
+        q1Status = q1Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q1Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        console.log(this + ' ' + q1b6State + ', ' + q1Status);
+    }
+});
+
+$("#q1b1, #q1b2, #q1b3, #q1b4, #q1b5, #q1b6").click(function() {
+    $("#aDebug").text("Q1 =" + q1Solution);
+    $("#Q2").show();
+    $('html, body').animate({
+            scrollTop: $("#Q2").offset().top
+        }, 1500);
+});
+
+//Q2 statements
+$("#q2b1").click(function() {
+
+    if (q2b1State == false && q2Status < 1) {
+        q2b1State = true;
+        q2Status = q2Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q2Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        console.log(this + ' ' + q2b1State + ', ' + q2Status);
+    }
+});
+
+$("#q2b2").click(function() {
+
+    if (q2b2State == false && q2Status < 1) {
+        q2b2State = true;
+        q2Status = q2Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q2Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q2b2State + ', ' + q2Status);
+    }
+
+});
+
+$("#q2b3").click(function() {
+
+    if (q2b3State == false && q2Status < 1) {
+        q2b3State = true;
+        q2Status = q2Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q2Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q2b3State + ', ' + q2Status);
+    }
+});
+
+$("#q2b4").click(function() {
+
+    if (q2b4State == false && q2Status < 1) {
+        q2b4State = true;
+        q2Status = q2Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q2Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q2b4State + ', ' + q2Status);
+    }
+});
+
+$("#q2b5").click(function() {
+
+    if (q2b5State == false && q2Status < 1) {
+        q2b5State = true;
+        q2Status = q2Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q2Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q2b5State + ', ' + q2Status);
+    }
+});
+
+$("#q2b6").click(function() {
+
+    if (q2b6State == false && q2Status < 1) {
+        q2b6State = true;
+        q2Status = q2Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q2Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q2b6State + ', ' + q2Status);
+    }
+});
+
+$("#q2b1, #q2b2, #q2b3, #q2b4, #q2b5, #q2b6").click(function() {
+
+    if (q2Status == 1) {
+        $("#aDebug").text("Q2 =" + q2Solution);
+        $("#Q3").show();
+        $('html, body').animate({
+            scrollTop: $("#Q3").offset().top
+        }, 1500);
+    }
+
+
+
+});
+
+//Q3 statements
+$("#q3b1").click(function() {
+
+    if (q3b1State == false) {
+        q3b1State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+        console.log(this + ' ' + q3b1State + ', ' + q3Status);
+    }
+
+    else {q3b1State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b1State + ', ' + q3Status);
+        };
+});
+
+$("#q3b2").click(function() {
+
+    if (q3b2State == false) {
+        q3b2State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b2State + ', ' + q3Status);
+    }
+
+    else {q3b2State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b2State + ', ' + q3Status);
+        }
+
+});
+
+$("#q3b3").click(function() {
+
+    if (q3b3State == false) {
+        q3b3State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b3State + ', ' + q3Status);
+    }
+
+    else {q3b3State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b3State + ', ' + q3Status);
+        }
+});
+
+$("#q3b4").click(function() {
+
+    if (q3b4State == false) {
+        q3b4State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b4State + ', ' + q3Status);
+    }
+
+    else {q3b4State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b4State + ', ' + q3Status);
+        }
+});
+
+$("#q3b5").click(function() {
+
+    if (q3b5State == false) {
+        q3b5State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b5State + ', ' + q3Status);
+    }
+
+    else {q3b5State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b5State + ', ' + q3Status);
+        }
+});
+
+$("#q3b6").click(function() {
+
+    if (q3b6State == false) {
+        q3b6State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b6State + ', ' + q3Status);
+    }
+
+    else {q3b6State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b6State + ', ' + q3Status);
+        }
+});
+
+$("#q3b7").click(function() {
+
+    if (q3b7State == false) {
+        q3b7State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b7State + ', ' + q3Status);
+    }
+    else {q3b7State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b7State + ', ' + q3Status);
+        }
+});
+
+$("#q3b8").click(function() {
+
+    if (q3b8State == false) {
+        q3b8State = true;
+        q3Status = q3Status + 1;
+        $(this).addClass('active');
+
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+
+
+        console.log(this + ' ' + q3b8State + ', ' + q3Status);
+    }
+    else {q3b8State = false;
+        q3Status = q3Status - 1;
+        $(this).removeClass('active');
+
+        var buttonContent = $(event.target).text();
+        //Need to remove the specific text from the array using this.
+
+        console.log(this + ' ' + q3b8State + ', ' + q3Status);
+        }
+});
+
+$("#endButton").click(function(){
+
+    $("#Q4").show();
+        $("#Q3").hide();
+        $("#Q2").hide();
+        $("#Q1").hide();
+        $("#intro").hide();
+        //Assess the final content in the array
+    });
+
+//You can cut out the rest of the left over box code
+
+/* $("#q3b1, #q3b2, #q3b3, #q3b4, #q3b5, #q3b6, #q3b7, #q3b8").click(function() {
+
+    if (q2Status == 1) {
+        $("#aDebug").text("Q2 =" + q3Solution);
+        $("#Q4").show();
+        $("#Q3").hide();
+        $("#Q2").hide();
+        $("#Q1").hide();
+        $("#intro").hide();
+        $('html, body').animate({
+            scrollTop: $("#Q4").offset().top
+        }, 1500);
+    }
+
+
+
+}); */
+
+
+//Big boxes, jesus
+
+//Row 1
+
+
+$("#box1").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+$("#box2").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+$("#box3").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+$("#box4").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+
+//Row 2
+
+$("#box5").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+$("#box6").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+$("#box7").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+$("#box8").click(function() {
+
+    if ($(this).hasClass('active-box')) {
+
+        $(this).removeClass('active-box');
+
+    } else {
+        $(this).addClass('active-box');
+        var buttonContent = $(event.target).text();
+        q3Solution.push(buttonContent);
+        console.log(buttonContent);
+    };
+
+});
+
+$("#q3NP").click(function() {
+
+    $("#aDebug").text("Q3 =" + q3Solution);
+
+});
+
+
+
+//The logic should let a user click a button and check how many buttons have been clicked. If under 2, let them click another or the same button, if exactly 2, kill the function until they click one that's active, which'll bring them back down to 1 or 0.
+
+//Make a template where only one button in a row can be clicked, and another where multiple can be.
+
+//Maybe the buttons can have a "true" class, and jquery can check each respective chapter's buttons for said 'true' buttons and their attatched number when pressing the "next page" ID.
+
+//In the background after the page is over (when someone presses next page) it can select the outcome based on what the user chose, and display it when the overall quiz is over.
